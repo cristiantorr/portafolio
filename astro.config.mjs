21dbdb4f883414b6ab987dbc89956bc6f5932c33
cristiantorr <ps4cristiantorr@gmail.com>
2025-03-30 14:40:12 -0500

    Se agrega perfil y sobre mí

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,16 @@
 
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+/* import robotsTxt from "astro-robots-txt"
 
+import robotsTxt from "astro-robots-txt"
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), robotsTxt()],
+  site: 'https://porfolio.dev/'
+})
+ */
 // https://astro.build/config
 export default defineConfig({
   output: "server",
